52ffa479627bb4e46b5248b0679bdadaba0e9c7b
nieqianqian <nieqianqian@manqian.com>
2019-12-02 20:52:10 +0800

    what:赋值训练代码

---
+++ assignment/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x int
+	x = 1
+	p := &x
+	*p = 156
+	var count [10]int
+	for i, _ := range count {
+		count[i] += 100
+	}
+
+	var i, j, k int
+	i, j, k = 3, 5, 10
+	fmt.Println(i, j, k)
+	fmt.Println(fib(100))
+}
+func fib(n int) int {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
+	return x
+}
