918be063b7dbbad20a02e755698fc2f193cb7add
nqq <nqq@aozsky.com>
2019-12-06 10:27:19 +0800

    what:go training

--- memory/main.go
+++ memory/main.go
@@ -1,15 +1,21 @@
 package main
 
+import "fmt"
+
 // import (
 // 	"os"
 // )
 
+var a int
+
 func main() {
-	names := []string{"abby", "nqq", "nieqianqian"}
-	s, sep := "", ""
-	for _, name := range names {
-		s += sep + name
-		sep = " "
-	}
+	a = 10
+	fmt.Println(a)
+	// names := []string{"abby", "nqq", "nieqianqian"}
+	// s, sep := "", ""
+	// for _, name := range names {
+	// 	s += sep + name
+	// 	sep = " "
+	// }
 
 }
