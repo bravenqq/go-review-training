8f9580872930bd8d664a6c9c4eb98a759250a840
nqq <nqq@aozsky.com>
2019-12-06 17:13:24 +0800

    what:内存分析实现

--- memory/main.go
+++ memory/main.go
@@ -1,21 +1,33 @@
 package main
 
-import "fmt"
-
-// import (
-// 	"os"
-// )
-
-var a int
-
 func main() {
-	a = 10
-	fmt.Println(a)
-	// names := []string{"abby", "nqq", "nieqianqian"}
-	// s, sep := "", ""
-	// for _, name := range names {
-	// 	s += sep + name
-	// 	sep = " "
-	// }
+	x := make([]int, 10)
+	// escape to heap,申请的内存过大
+	y := make([]int, 20000)
+	l := 10
+	// escap to heap，不确定大小
+	d := make([]int, l)
+	c := make(map[string]string)
+	names := []string{"nqq", "hxw"}
+	s := ""
+	for _, name := range names {
+		s += name + " "
+	}
+	f := Fibonacii
+}
 
+func Fibonacii() func() int {
+	// escape to heap 函数结束时变量仍被别的函数使用
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+func incr(x int) *int {
+	x += 1
+
+	//escape to heap,函数结束后反回了地址
+	return &x
 }
