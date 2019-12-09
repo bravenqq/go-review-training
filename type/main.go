3d44f284180e152106c41bc6a72469046fe3b111
nqq <nqq@aozsky.com>
2019-12-09 14:39:33 +0800

    what:完成类型

---
+++ type/main.go
@@ -0,0 +1,22 @@
+// Package main provides ...
+package main
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsolutedZeroC Celsius = -273.15
+	FreezingC      Celsius = 0
+	BoilingC       Celsius = 100
+)
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius(f-32) * 5 / 9 }
+
+func main() {
+	freezing := CToF(FreezingC)
+	fmt.Println("the temperature is:", freezing)
+}
