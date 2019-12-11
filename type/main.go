210d0ff666ea81252e1366f309036c2c907fef88
nqq <nqq@aozsky.com>
2019-12-11 12:35:02 +0800

    what:添加开尔文温度

--- type/main.go
+++ type/main.go
@@ -5,6 +5,7 @@ import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvins float64
 
 const (
 	AbsolutedZeroC Celsius = -273.15
@@ -12,9 +13,18 @@ const (
 	BoilingC       Celsius = 100
 )
 
+const (
+	AbsolutedZero Kelvins = 0
+	Freezing      Kelvins = 273.15
+	Boiling       Kelvins = 373.15
+)
+
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 func FToC(f Fahrenheit) Celsius { return Celsius(f-32) * 5 / 9 }
+
+func KToC(k Kelvins) Celsius { return Celsius(float64(k) - float64(Freezing)) }
+func CToK(c Celsius) Kelvins { return Kelvins(float64(c) + float64(Freezing)) }
 
 func main() {
 	freezing := CToF(FreezingC)
