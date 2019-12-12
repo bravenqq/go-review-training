97a97f4339f4e932cbf1c43e4f5c3804e0476575
nieqianqian <nieqianqian@manqian.com>
2019-12-12 22:03:46 +0800

    what:总结了整数类型的存储方式

---
+++ int/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//x在计算机中的存储形式补码，值为：11111111
+	var x int8 = -1
+	//右移动1位值为：11111111，补码值，转换为原码值为：10000001即10进制-1
+	y := x >> 1
+	//左移之后的存储值为11111100,原码值为：10000100即10进制-4
+	z := x << 2
+	fmt.Printf("%d\n", y)
+	fmt.Println(z)
+	var a uint = 64
+	//i类型uint, 值一直非负数，导致死循环
+	for i := a; i >= 0; i-- {
+		fmt.Println(i)
+	}
+}
