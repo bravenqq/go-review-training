be9c32ccdfaaef4d9b745c14eb26dc06b2848a70
nieqianqian <nieqianqian@manqian.com>
2019-12-18 19:36:59 +0800

    what:修改字符串与[]byte的比较

---
+++ strvsbyte/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	const sample = "\xbd\xb2\x3d\xbc\x20\xe2\x8c\x98"
+	fmt.Println("Println:")
+	fmt.Println(sample)
+	fmt.Println("Byte loop:")
+	for i := 0; i < len(sample); i++ {
+		fmt.Printf("%x ", sample[i])
+	}
+	fmt.Println()
+	fmt.Println("Printf with %x:")
+	fmt.Printf("%x\n", sample)
+	fmt.Println("Printf with %q:")
+	fmt.Printf("%q\n", sample)
+	fmt.Println("Printf with %+q:")
+	fmt.Printf("%+q\n", sample)
+
+	data := []byte(sample)
+	fmt.Println("Byte loop:")
+	for i := 0; i < len(data); i++ {
+		fmt.Printf("%q ", data[i])
+	}
+	fmt.Println()
+
+	fmt.Println("rune loop:")
+	for index, runeValue := range sample {
+		fmt.Printf("%#U starts at byte position %d\n", runeValue, index)
+	}
+	data = []byte{16, 15, 10}
+}
