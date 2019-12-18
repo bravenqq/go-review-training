71f66225eca08945bff4ce7c60272f26dfcc6671
nieqianqian <nieqianqian@manqian.com>
2019-12-18 10:39:29 +0800

    what:总结比较了string与[]byte

--- byte/main.go
+++ byte/main.go
@@ -4,37 +4,33 @@ package main
 import "fmt"
 
 func main() {
-
 	//计算机中存储的是字符对应码点编码之后的值
+	//在编辑器中使用设置的utf-8解码序列值展示成'a','b','c'字符
 	data := []byte{'a', 'b', 'd'}
 	fmt.Printf("data len:%d cap:%d\n", len(data), cap(data))
 	for _, c := range data {
 		fmt.Printf("char:%c code:%d type:%T\n", c, c, c)
 	}
-
-	data[2] = 'c'
-
-	str := []rune{'聂', '倩', '倩'}
-	fmt.Printf("str len:%d cap:%d\n", len(str), cap(str))
-	for _, c := range str {
-		fmt.Printf("char:%c code:%d type:%T\n", c, c, c)
+	fmt.Println("------------------------------------------------")
+	//s字符串中存储的是字符对应码点编码之后的值
+	//在编辑器中使用设置的utf-8解码序列值展示成"abc"字符串
+	s := "abc"
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("char:%c code:%d type:%T\n", s[i], s[i], s[i])
 	}
-
-	s := "聂倩倩"
-	//s的底层仍然是[]byte类型，所以len函数是按byte字节计算长度
-	fmt.Printf("s len:%d\n", len(s))
-
-	//将string转换成str之后按rune类型去长度
-	str = []rune(s)
-	fmt.Printf("str len:%d\n", len(str))
-
-	s = "abc"
-	data = []byte(s)
-	data[0] = 'd'
-	for _, c := range data {
-		fmt.Printf("char:%c code:%d type:%T\n", c, c, c)
+	fmt.Printf("s addr:%p\n", &s)
+	fmt.Println("------------------------------------------------")
+	//此处copy data中的值到新的内存中
+	//s指向新的内存
+	s = string(data)
+	fmt.Printf("s addr:%p\n", &s)
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("char:%c code:%d type:%T\n", s[i], s[i], s[i])
 	}
-
-	fmt.Println("s:", s)
-
+	fmt.Println("------------------------------------------------")
+	//由于s = string(data)是开辟了新内存copy data值，所以修改data不会改变s的值
+	data[0] = 'e'
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("char:%c code:%d type:%T\n", s[i], s[i], s[i])
+	}
 }
