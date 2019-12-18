36be27d355aa3d6c3712a08a43c1a24cfe74269f
nieqianqian <nieqianqian@manqian.com>
2019-12-18 11:12:16 +0800

    what:添加[]rune代码测试

--- rune/main.go
+++ rune/main.go
@@ -3,11 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	a := '聂'
-	fmt.Printf("%d %T\n", a, a)
-	b := 'a'
-	fmt.Printf("%d %T\n", b, b)
-	c := "聂"
-	fmt.Printf("c length: %d\n", len(c))
-	fmt.Printf("c length: %d\n", len([]rune(c)))
+
+	//rune是int32的别名
+	//存储字符码点编码之后的值
+	//与[]byte只能存储占用一字节的英文字符不同，rune可以任意的unicode集中字符
+	r := []rune{'a', 'b', 'c', 'd', '聂', '倩'}
+	fmt.Println("r len:", len(r))
+	for _, c := range r {
+		fmt.Printf("rune:%c code:%d type:%T\n", c, c, c)
+	}
+
+	s := string(r)
+	//len(s)是计算字符串占用的字节数
+	//在utf-8编码中中文字符占用3字节所以len(s)是10
+	fmt.Println("s len:", len(s))
+
+	//s[i]也是通过一字节一字节访问数据的
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("s:%c code:%d type:%T\n", s[i], s[i], s[i])
+	}
+
+	//range s是通过rune类型为单位来遍历字符串的数据的
+	for _, c := range s {
+		fmt.Printf("rune:%c code:%d type:%T\n", c, c, c)
+	}
 }
