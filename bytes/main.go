28371ddf5ab3b89b75dc91f6e80de5b31e629ad3
nieqianqian <nieqianqian@manqian.com>
2019-12-27 13:26:43 +0800

    what:提交实验代码

--- bytes/main.go
+++ bytes/main.go
@@ -2,9 +2,34 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	bytes.
+	buffer := bytes.NewBuffer(nil)
+
+	f, err := os.Open("./test")
+	if err != nil {
+		log.Fatalf("open file test err:%v\n", err)
+	}
+	defer f.Close()
+
+	go func() {
+		for {
+			_, err := buffer.ReadFrom(f)
+			if err == io.EOF {
+				break
+			}
+		}
+	}()
+
+	for {
+		_, err = buffer.WriteTo(os.Stdout)
+		if err == io.EOF {
+			break
+		}
+
+	}
 }
