c44e1e6e453ed220522322bb67811210df7afa92
nqq <nqq@aozsky.com>
2020-01-19 21:29:09 +0800

    what:第一章第一节测试

---
+++ exercise/ch1/1.3/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkJoin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Join([]string{"s", "b"})
+	}
+}
+
+func BenchmarkFastJoin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		FastJoin([]string{"s", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"})
+	}
+}
+
+func BenchmarkAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Add(1, 2)
+	}
+}
