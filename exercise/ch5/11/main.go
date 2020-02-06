39f9dbc345627fa71f29079c840142fa58b79d03
nqq <nqq@aozsky.com>
2020-02-06 13:35:31 +0800

    what:完成习题11

---
+++ exercise/ch5/11/main.go
@@ -0,0 +1,90 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 136.
+
+// The toposort program prints the nodes of a DAG in topological order.
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"sort"
+)
+
+//!+table
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms":     {"data structures"},
+	"calculus":       {"linear algebra"},
+	"linear algebra": {"calculus"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+//!-table
+
+//!+main
+func main() {
+	order, err := topoSort(prereqs)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, course := range order {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string][]string) ([]string, error) {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	var isDirected func(item string, items []string) bool
+	isDirected = func(item string, items []string) bool {
+		for _, it := range items {
+			if item == it {
+				fmt.Printf("%s\t%s互为前置课程\n", item, it)
+				return true
+			}
+			return isDirected(item, m[it])
+		}
+		return false
+	}
+	var keys []string
+	for key := range m {
+		if isDirected(key, m[key]) {
+			return nil, errors.New("存在互相循环的前置课程")
+		}
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	visitAll(keys)
+	return order, nil
+}
+
+//!-main
