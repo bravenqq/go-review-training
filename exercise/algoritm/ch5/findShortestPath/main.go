8fb08b832028738d583df9d391cd2ba3faaff086
nqq <nqq@aozsky.com>
2020-02-07 16:35:26 +0800

    what:利用广度优先搜索查找最短路径长度

---
+++ exercise/algoritm/ch5/findShortestPath/main.go
@@ -0,0 +1,39 @@
+/*
+查找从A点到F点的最短路径是多少
+*/
+
+package main
+
+import "fmt"
+
+var paths = map[string][]string{
+	"a": {"b", "d"},
+	"b": {"c"},
+	"d": {"g", "e"},
+	"g": {"c"},
+	"e": {"c"},
+	"c": {"f"},
+}
+
+func main() {
+	near := paths["a"]
+	var path int
+	var fund bool
+	for len(near) != 0 {
+		temp := near
+		near = nil
+		path++
+		for _, p := range temp {
+			if p == "f" {
+				fund = true
+				break
+			}
+			if pt, res := paths[p]; res {
+				near = append(near, pt...)
+			}
+		}
+	}
+	if fund {
+		fmt.Println("找到了最短路径，长度是：", path)
+	}
+}
