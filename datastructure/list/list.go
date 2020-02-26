92b2e37041dd47eaf719dfc7e0894fa1a273179c
nqq <nqq@aozsky.com>
2020-02-26 19:51:20 +0800

    what:实现双向链表

---
+++ datastructure/list/list.go
@@ -0,0 +1,79 @@
+// Package list provides ...
+package list
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrNotFound = errors.New("node not found")
+	ErrEmpty    = errors.New("link list is empty")
+)
+
+//Node node of list
+type Node struct {
+	Data int
+	next *Node
+	pre  *Node
+}
+
+//LinkList is a kind of data structure which feature is FIFO
+type LinkList struct {
+	head *Node
+	tail *Node
+}
+
+//NewLinkList return a null LinkList
+func NewLinkList() *LinkList {
+	return &LinkList{head: nil, tail: nil}
+}
+
+//Add add a node
+func (l *LinkList) Insert(n Node) {
+	if l.head == nil {
+		l.head = &n
+		l.tail = &n
+		return
+	}
+	n.pre = l.tail
+	l.tail.next = &n
+	l.tail = &n
+
+}
+
+//Remove remove a node
+func (l *LinkList) Remove(n Node) error {
+	if l.head.Data == n.Data {
+		l.head = l.head.next
+		return nil
+	}
+	node := l.head
+	for node != nil {
+		if node.next.Data == n.Data {
+			node.next = node.next.next
+			return nil
+		}
+		node = node.next
+	}
+	return ErrNotFound
+}
+
+//Pop pop a node from the tail
+func (l *LinkList) Pop() (Node, error) {
+	if l.tail.pre == nil {
+		return Node{}, ErrEmpty
+	}
+	node := l.tail
+	l.tail = l.tail.pre
+	return *node, nil
+}
+
+//Display display all data of LinkList
+func (l *LinkList) Display() {
+	node := l.head
+	for node != nil {
+		fmt.Printf("%d\t", node.Data)
+		node = node.next
+	}
+}
