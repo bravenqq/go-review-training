1ecbdfde295067a06b7a3024838a473c45cea0f7
nqq <nqq@aozsky.com>
2020-03-07 16:20:05 +0800

    what:完成习题8.8

---
+++ exercise/ch8/8.8/reverb.go
@@ -0,0 +1,81 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 224.
+
+// Reverb2 is a TCP server that simulates an echo.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+	wg.Done()
+}
+
+//!+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	input := bufio.NewScanner(c)
+	lines := make(chan string)
+	go func(lines chan<- string) {
+		for input.Scan() {
+			lines <- input.Text()
+		}
+
+		if input.Err() != nil {
+			log.Println(input.Err())
+		}
+	}(lines)
+
+	go func() {
+		wg.Wait()
+	}()
+
+	timeout := 10 * time.Second
+	ticker := time.NewTicker(timeout)
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("time out")
+			ticker.Stop()
+			return
+		case text := <-lines:
+			ticker.Stop()
+			ticker = time.NewTicker(timeout)
+			wg.Add(1)
+			go echo(c, text, 1*time.Second)
+		}
+	}
+}
+
+//!-
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
---
+++ exercise/ch8/select/main.go
@@ -0,0 +1,17 @@
+// Package main provides ...
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 1)
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x)
+		case ch <- i:
+
+		}
+	}
+
+}
