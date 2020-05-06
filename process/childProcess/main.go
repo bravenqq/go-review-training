2ed07fd9388af8622f458500666e3466bb4e4622
bantana <bantana@gmail.com>
2020-05-06 20:49:06 +0800

    what:go 中fork and exec but not wait

--- process/childProcess/main.go
+++ process/childProcess/main.go
@@ -9,7 +9,10 @@ import (
 func main() {
 	cmd := exec.Command("sleep", "3600")
 	//fork and exec sleep,wait sleep to finsh
-	err := cmd.Run()
+	// err := cmd.Run()
+
+	//fork and exec sleep,but don't wait sleep to finsh
+	err := cmd.Start()
 	if err != nil {
 		log.Println("err:", err)
 	}
