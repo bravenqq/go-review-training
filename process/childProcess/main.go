8837d306b252c399347c3b10f90819ffeeb83d55
bantana <bantana@gmail.com>
2020-05-06 21:34:22 +0800

    what:使用exec.Run启动两个进程

--- process/childProcess/main.go
+++ process/childProcess/main.go
@@ -2,18 +2,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os/exec"
 )
 
 func main() {
-	cmd := exec.Command("sleep", "3600")
+	cmd := exec.Command("find", "/Users/abbynie")
 	//fork and exec sleep,wait sleep to finsh
-	// err := cmd.Run()
-
-	//fork and exec sleep,but don't wait sleep to finsh
-	err := cmd.Start()
+	err := cmd.Run()
 	if err != nil {
 		log.Println("err:", err)
+		fmt.Fprintln(cmd.Stderr)
 	}
+	fmt.Printf("%+v", cmd.Process)
+	c := exec.Command("find", "/Users/abbynie")
+
+	//fork and exec sleep,wait sleep to finsh
+	err = c.Run()
+	if err != nil {
+		log.Println("err:", err)
+		fmt.Fprintln(c.Stderr)
+	}
+	fmt.Printf("%+v", c.Process)
+
+	//fork and exec sleep,but don't wait sleep to finsh
+	// err := cmd.Start()
+	// if err != nil {
+	// 	log.Println("err:", err)
+	// }
+	// fmt.Printf("%+v", cmd.Process)
 }
