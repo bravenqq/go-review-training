cbfa9b2f0c5300efbaae2e0fe05ada15d518b445
bantana <bantana@gmail.com>
2020-05-06 22:03:42 +0800

    what:一个进程使用exec.Start,一个用exec.Run同时启动两个进程处理任务，并且父进程等待子进程执行完

--- process/childProcess/main.go
+++ process/childProcess/main.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os/exec"
@@ -10,21 +11,22 @@ import (
 func main() {
 	cmd := exec.Command("find", "/Users/abbynie")
 	//fork and exec sleep,wait sleep to finsh
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
 	err := cmd.Start()
 	if err != nil {
-		log.Println("err:", err)
-		fmt.Fprintln(cmd.Stderr)
+		log.Println("err:", err, ":", stderr.String())
 	}
-	fmt.Printf("%+v", cmd.Process)
-	c := exec.Command("find", "/Users/abbynie")
+	fmt.Printf("%+v\n", cmd.Process)
 
+	c := exec.Command("find", "/Users/abbynie")
+	c.Stderr = &stderr
 	//fork and exec sleep,wait sleep to finsh
-	err = c.Start()
+	err = c.Run()
 	if err != nil {
-		log.Println("err:", err)
-		fmt.Fprintln(c.Stderr)
+		log.Println("err:", err, ":", stderr.String())
 	}
-	fmt.Printf("%+v", c.Process)
+	fmt.Printf("%+v\n", c.Process)
 
 	//fork and exec sleep,but don't wait sleep to finsh
 	// err := cmd.Start()
