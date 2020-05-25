f0031931a353758b2745215520b93f5994a8790c
nqq <nqq@aozsky.com>
2020-05-25 11:10:03 +0800

    what:自动售卖机实现售卖商品的功能

--- vedingMachine/main.go
+++ vedingMachine/main.go
@@ -1,6 +1,7 @@
 // Package main provides ...
 package main
 
+//state 自动售卖机的行为
 type state interface {
 	selectItem(count int) error
 	giveMoney(money float32) error
@@ -8,6 +9,7 @@ type state interface {
 	addItem(count int) error
 }
 
+//VendingMachine 自动售卖机，售卖商品
 type VendingMachine struct {
 	hasItem      state
 	ruquestItem  state
@@ -15,5 +17,30 @@ type VendingMachine struct {
 	hasnoItem    state
 	itemCount    int
 	itemPrice    float32
+	count        int //顾客请求商品数量
 	currentState state
+}
+
+func (vm *VendingMachine) selectItem(count int) error {
+	return vm.currentState.selectItem(count)
+}
+
+func (vm *VendingMachine) giveMoney(money float32) error {
+	return vm.currentState.giveMoney(money)
+}
+
+func (vm *VendingMachine) giveItem() error {
+	return vm.currentState.giveItem()
+}
+
+func (vm *VendingMachine) addItem(count int) error {
+	return vm.addItem(count)
+}
+
+func (vm *VendingMachine) set(s state) {
+	vm.currentState = s
+}
+
+func (vm *VendingMachine) incrmentItem(count int) {
+	vm.itemCount += count
 }
