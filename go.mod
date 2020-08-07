b0d6c4b130cc764a72941cab4b4c1b5b1be9a213
nqq <nqq@aozsky.com>
2020-08-07 19:48:02 +0800

    what:实现LogHandler中间键

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/golangci/golangci-lint v1.23.8 // indirect
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/securego/gosec v0.0.0-20200301204537-7525fe4bb7b0
+	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
--- middleware/main.go
+++ middleware/main.go
@@ -10,10 +10,19 @@ import (
 
 //Log log requst url middleware
 func Log(h http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		log.Println("request url:", req.URL.String())
 		start := time.Now()
-		h(resp, req)
+		h(w, req)
+		log.Println("spend time:", time.Now().Sub(start))
+	})
+}
+
+func LogHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Println("request url ", req.URL.RequestURI())
+		start := time.Now()
+		h.ServeHTTP(w, req)
 		log.Println("spend time:", time.Now().Sub(start))
 	})
 }
@@ -21,9 +30,18 @@ func Log(h http.HandlerFunc) http.HandlerFunc {
 func main() {
 	addr := flag.String("addr", ":8080", "please input addr")
 	mux := http.NewServeMux()
-	mux.HandleFunc("/index", Log(func(resp http.ResponseWriter, req *http.Request) {
-		resp.Write([]byte("hello world"))
+	mux.HandleFunc("/index", Log(func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("hello world"))
 	}))
+	mux.Handle("/hello", LogHandler(Person{"nqq"}))
 	log.Println("start server addr ", *addr)
 	log.Fatal(http.ListenAndServe(*addr, mux))
+}
+
+type Person struct {
+	Name string
+}
+
+func (p Person) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello " + p.Name))
 }
