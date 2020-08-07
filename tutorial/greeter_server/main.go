1a2ae4063c5d19583721498ef926694941ce88a9
nqq <nqq@aozsky.com>
2020-08-07 10:21:23 +0800

    what:实现服务端

---
+++ tutorial/greeter_server/main.go
@@ -0,0 +1,34 @@
+// Package main provides ...
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "iohttps.com/nqq/go-review-training/tutorial/helloworld"
+)
+
+const (
+	port = ":8080"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReplay, error) {
+	log.Println("Received:", in.GetName())
+	return &pb.HelloReplay{Message: "Hello" + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalln("faild to listen:", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	s.Serve(lis)
+}
