ed4351ffad5a8cffca87447937fdc097ee92be89
nqq <nqq@qqmini.local>
2020-08-16 16:36:28 +0800

    what:在一个程序中实现多端口监听服务

--- httptest/main.go
+++ httptest/main.go
@@ -1,11 +1,33 @@
 // Package main provides ...
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	router := http.NewServeMux()
-	router.Handle()
+	router.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("welcome to index"))
+	}))
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+	go func() {
+		err := server.ListenAndServe()
+		log.Println("listen port 8080")
+		if err != nil {
+			log.Fatal("listen port:8080 err:", err)
+		}
+	}()
 
-	http.ListenAndServe()
+	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("./"))))
+	log.Println("listen port 8090")
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		log.Fatal("listen port:8090 err:", err)
+	}
+
 }
