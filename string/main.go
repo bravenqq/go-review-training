aaf30a20293644c2c90d759b664a3e3704526d3a
nqq <nqq@aozsky.com>
2021-02-23 09:58:44 +0800

    up

--- string/main.go
+++ string/main.go
@@ -10,43 +10,12 @@ func comma(s string) string {
 	return comma(s[:n-3]) + "," + s[n-3:]
 }
 func main() {
-	// fmt.Println(comma("123456"))
-	str := "你好，中国"
-	for _, s := range str {
-		fmt.Printf("%d ", s)
+	str := "h好，中国"
+	bytes := []rune(str)
+	for _, s := range bytes {
+		fmt.Printf("%b ", s)
 	}
-	// fmt.Println()
-	// for _, s := range str {
-	// 	fmt.Printf("%s ", s)
-	// /  }
-	fmt.Println()
-	bytes := []byte(str)
-	fmt.Println(bytes)
-	// runes := []rune(str)
-	// fmt.Println(string(runes[:3]))
-	// str := "hello,world"
-	// c := str
-	// fmt.Printf("str:%p str:%p\n", &str, &c)
-	// Out(c)
-	// c += "!"
-	// Out(c)
-	//
-	// c += "!"
-	// fmt.Printf("%p %p\n", &str, &c)
-	//
-	// subStr := str[:5]
-	// fmt.Printf("subStr %s, str %s\n", subStr, str)
-	//
-	// str = "hi,world"
-	// fmt.Printf("subStr %s, str %s\n", subStr, str)
-	//
-	// subStr = "hi,nqq"
-	// fmt.Printf("subStr %s, str %s\n", subStr, str)
-	//
-	// str = string([]byte{2, 3})
-	// fmt.Println(str)
-	//改变字符串底层值，报错cannot assign to subStr[:5]
-	//subStr[:5] = "hi"
+
 }
 
 func Out(s string) {
