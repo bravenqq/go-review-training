19cd3ec7e19237b31d011990de3dcb3c10a9f23b
nqq <nqq@aozsky.com>
2021-02-25 15:00:04 +0800

    feat(test): commit 观察slice的内存分配

--- slice2/main.go
+++ slice2/main.go
@@ -1,18 +1,15 @@
 // Package main provides ...
 package main
 
-import "fmt"
-
 func main() {
 	var slice []int
 	//slice 与nil做比较，底层数组没分配内存
-	if slice == nil {
-		fmt.Println("slice is nil")
-	}
-
 	a := 10
 	slice = make([]int, a, 10)
-	if slice == nil {
-		fmt.Println("slice still nil")
-	}
+
+	slice = f()
+}
+
+func f() []int {
+	return make([]int, 0, 10)
 }
