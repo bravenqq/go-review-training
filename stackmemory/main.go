4c57659ab713dfe9ee1777a372d3741472befdde
nqq <nqq@aozsky.com>
2021-02-25 09:19:51 +0800

    feat(stack): commit 循环中的字符串内存分析

--- stackmemory/main.go
+++ stackmemory/main.go
@@ -2,12 +2,26 @@
 package main
 
 func main() {
-	var a = 1
-	f(a)
+	strs := []string{"hell", "wor", "test", "nqq"}
+	// for _, s := range strs {
+	// 	f(s)
+	// }
+	Join(strs, ",")
 }
 
-func f(t int) *int {
-	//使用的是main中a的地址来修改a变量
-	t++
-	return &t
+func Join(slice []string, str string) string {
+
+	var s, sep string
+	for i := 0; i < len(slice); i++ {
+		f(sep + slice[i]) //此处sep+slice[i] does not escape
+		//此处sep+slice escape逃逸了，
+		//1.s+=sep+slice[i]=>tmp = sep+slice[i],s = tmp
+		//2. 一次循环结束后循环内变量sep+slice[i]的内存要回收，但是sep+slice[i]被外部变量s引用了，所以将sep+slice[i]分配到堆中
+
+		s += sep + slice[i]
+		sep = str
+	}
+	return s
+}
+func f(s string) {
 }
