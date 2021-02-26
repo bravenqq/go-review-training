110b4fa3e4c4421890403e3ec1dfe026630eba63
nqq <nqq@aozsky.com>
2021-02-26 13:51:16 +0800

    feat(test): commit slice 分配到heap上

--- slice2/main.go
+++ slice2/main.go
@@ -2,19 +2,12 @@
 package main
 
 func main() {
-	a := 10
-	b := 20
-	//make([]int, a, b) escapes to heap
-	slice := make([]int, a, b)
+	slice := f()
 	_ = slice
 }
 
-func add(slice []int, v int) []int {
-	l := cap(slice)
-	for i := 0; i < l+5; i++ {
-		slice = append(slice, v)
-	}
-
-	// fmt.Printf("slice:%p \n", &slice)
+func f() []int {
+	//make([]int, 10, 20) escapes to heap
+	slice := make([]int, 10, 20)
 	return slice
 }
