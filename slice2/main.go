b098d6d166fe265c851555e83cbeff54bde642c4
nqq <nqq@aozsky.com>
2021-02-26 10:17:04 +0800

    feat(test): commit slice与nil的关系

--- slice2/main.go
+++ slice2/main.go
@@ -1,10 +1,18 @@
 // Package main provides ...
 package main
 
+import "fmt"
+
 func main() {
 	var slice []int
 	//slice 与nil做比较，底层数组没分配内存
-	slice = make([]int, 5, 10)
+	if slice == nil {
+		fmt.Println("slice is nil")
+	}
+	slice = make([]int, 0, 10)
+	if slice == nil {
+		fmt.Println("slice still nil")
+	}
 }
 
 func f() []int {
