73bc6183cb0afca83f8a870ad95dd9f2cf44aad0
nqq <nqq@aozsky.com>
2021-03-01 17:05:55 +0800

    feat(test): commit struct

--- struct/main.go
+++ struct/main.go
@@ -1,46 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"unsafe"
-)
+type Person struct {
+	Name string
+	Age  int
+}
+
+type Employee struct {
+	Person
+	ID     int
+	Salary int
+}
 
 func main() {
-	var slice []byte
-	var a struct{}
-	var n Node
-	fmt.Println("slice size: ", unsafe.Sizeof(slice))
-	fmt.Println("a size:", unsafe.Sizeof(a))
-	fmt.Println("n size:", unsafe.Sizeof(n))
+	var p Employee
+	p.Name = "nqq"
 
-	// fillstruct
-
-	var nqq = Person{
-		Name: "fesf",
-		Age:  0,
-	}
-
-	fmt.Println(nqq)
-	// anonymouse struct | function
-
-	var abbynie = struct {
-		Name string
-		age  int
-	}{
-		"niexiaoqian",
-		16,
-	}
-
-	fmt.Println(abbynie)
+	e := new(Employee)
+	e.Name = "test"
 
 }
 
 type Node struct {
 	next *Node
 	Data Person
-}
-
-type Person struct {
-	Name string
-	Age  int
 }
