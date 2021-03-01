391558312c6f75c28700e99f611bea032f0a3953
nqq <nqq@aozsky.com>
2021-03-01 14:39:39 +0800

    feat(test): commit map内存stack,heap分配

--- map/main.go
+++ map/main.go
@@ -1,16 +1,17 @@
 // Package main provides ...
 package main
 
-import "fmt"
-
 func main() {
-	var m map[int]string
-	m = make(map[int]string)
-	m[1] = "hello"
-	fmt.Println(m[1])
-	fmt.Println(m[2])
-	// if m == nil {
-	// 	fmt.Println("map is nil")
-	// 	fmt.Println("map count:", len(m))
-	// }
+	// var m map[int]string
+	// m = make(map[int]string)
+	// m[1] = "hello"
+	m := f()
+	_ = m
+	// fmt.Println(m[1])//world
+}
+
+func f() map[int]string {
+	m := make(map[int]string)
+	m[1] = "world"
+	return m
 }
