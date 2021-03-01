10ee83e6aaea081cba56e834cde2371275937023
nqq <nqq@aozsky.com>
2021-03-01 14:14:17 +0800

    feat(test): commit 测试map取值

--- map/main.go
+++ map/main.go
@@ -5,8 +5,12 @@ import "fmt"
 
 func main() {
 	var m map[int]string
-	if m == nil {
-		fmt.Println("map is nil")
-		fmt.Println("map count:", len(m))
-	}
+	m = make(map[int]string)
+	m[1] = "hello"
+	fmt.Println(m[1])
+	fmt.Println(m[2])
+	// if m == nil {
+	// 	fmt.Println("map is nil")
+	// 	fmt.Println("map count:", len(m))
+	// }
 }
