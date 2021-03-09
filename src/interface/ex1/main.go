02c4e066c33fb64850ec92b56b1d275b710d7952
nqq <nqq@aozsky.com>
2021-03-09 13:34:55 +0800

    up

--- src/interface/ex1/main.go
+++ src/interface/ex1/main.go
@@ -11,12 +11,15 @@ import (
 type Person struct {
 }
 
+//声明了一个名称为Printer的接口
 type Printer interface {
 	Print()
 }
 
 func main() {
+	//io.Writer接口类型变量w
 	var w io.Writer
+	//
 	fmt.Printf("%T\n", w)
 	w = os.Stdout
 	fmt.Printf("%T\n", w)
