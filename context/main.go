82dd369d681ecd5c8fa887d76ab27ea0c14e29b4
nqq <nqq@aozsky.com>
2021-04-06 19:49:42 +0800

    feat(training): commit 使用context.Deadline控制goroutine结束

--- context/main.go
+++ context/main.go
@@ -12,7 +12,7 @@ import (
 //2. 使用context实现超时取消
 func main() {
 	tasks := make(chan int)
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(5*time.Second))
 	defer cancel()
 	for i := 0; i < 10; i++ {
 		go func(ctx context.Context) {
@@ -34,5 +34,4 @@ func main() {
 		}
 	}()
 	time.Sleep(10 * time.Second)
-
 }
